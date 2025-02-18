29eaad6e7865c01ea76cfdb356e47c89d876b761
Maulik shah <maulik_shah@hotmail.co.in>
2025-02-18 14:16:14 +0530

    First Commit.

---
+++ DownloadImages/DownloadImagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DownloadImagesApp.swift
+//  DownloadImages
+//
+//  Created by Maulik Shah on 2/18/25.
+//
+
+import SwiftUI
+
+@main
+struct DownloadImagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            DownloadingImages()
+        }
+    }
+}
---
+++ DownloadImages/Views/DownloadingImages.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DownloadImages
+//
+//  Created by Maulik Shah on 2/18/25.
+//
+
+import SwiftUI
+
+struct DownloadingImages: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    DownloadingImages()
+}
